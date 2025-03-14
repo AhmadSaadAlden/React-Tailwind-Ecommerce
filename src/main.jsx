a697ddb43d21aa05806ef44d87e2e08c81f94e6b
AhmadSaadAlden <ahmadsaad01045@gmail.com>
2025-03-14 22:01:24 +0300

    config3

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App basename= "/React-Tailwind-Ecommerce/" />
+    <HashRouter>
+      <App basename= "/React-Tailwind-Ecommerce/" />
+    </HashRouter>
   </StrictMode>,
 )
