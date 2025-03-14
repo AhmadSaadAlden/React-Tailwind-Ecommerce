2980da413c852a5bee20b13bb27189e1ab237ee4
AhmadSaadAlden <ahmadsaad01045@gmail.com>
2025-03-14 21:01:43 +0300

    add basename and congiguration

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import "slick-carousel/slick/slick-theme.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App basename= "/React-Tailwind-Ecommerce/" />
   </StrictMode>,
 )
