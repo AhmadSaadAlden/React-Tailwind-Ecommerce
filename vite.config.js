7660208a764931558d6c2f646853a306228db6cc
AhmadSaadAlden <ahmadsaad01045@gmail.com>
2025-03-14 21:34:23 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Tailwind-Ecommerce",
+  base: "/React-Tailwind-Ecommerce/",
   build: {
     outDir: "build"
   }
